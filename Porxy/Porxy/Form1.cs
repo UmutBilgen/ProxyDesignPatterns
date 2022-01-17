0abd983be9eb0eec06a1e4b08312493532cceb74
Umut Bilgen <84351356+UmutBilgen@users.noreply.github.com>
2022-01-17 18:28:05 +0300

    Proxy Dessing Patterns Örneği
    Proxy Dessing Patterns Örneği

---
+++ Porxy/Porxy/Form1.cs
@@ -0,0 +1,29 @@
+﻿using Porxy.PorxyDP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Porxy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IKullaniciBilgi proxy = new ProxyKullaniciBilgi();
+            MessageBox.Show(proxy.getUserInformation());
+            MessageBox.Show("AHAHAHAH");
+            MessageBox.Show(proxy.getUserInformation());
+        }
+    }
+}
---
+++ Porxy/Porxy/PorxyDP/ProxyKullaniciBilgi.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Porxy.PorxyDP
+{
+    class ProxyKullaniciBilgi:IKullaniciBilgi
+    {
+     private BTKKullaniciBilgileri objeBilgi;
+        private Boolean LoadRealObje;
+        private string userinfo = string.Empty;
+
+        private string getUserInformationFromBTK()
+        {
+            try
+            {
+                objeBilgi = new BTKKullaniciBilgileri();
+                LoadRealObje = true;
+                userinfo = objeBilgi.getUserInformation();
+                return userinfo;
+            }
+            catch
+            {
+                LoadRealObje =false;
+                return "";
+
+            } 
+           
+        }
+        public string getUserInformation()
+        {
+            LoadRealObje = false;
+            string result = "";
+            if(objeBilgi==null)
+            {
+                result = getUserInformationFromBTK();
+            }
+            if (!LoadRealObje)
+                return userinfo+"  Gerçek Obje Değil";
+            else
+            {
+                return result;
+            }
+            
+        }
+
+    }
+}
